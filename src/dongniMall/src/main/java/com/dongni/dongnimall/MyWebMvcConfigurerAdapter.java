1eb39560622917be84c0faf56ee7d7e7e28a33e2
ZS140 <859691959@qq.com>
2019-09-25 17:59:15 +0800

    接口文档

--- src/dongniMall/src/main/java/com/dongni/dongnimall/MyWebMvcConfigurerAdapter.java
+++ src/dongniMall/src/main/java/com/dongni/dongnimall/MyWebMvcConfigurerAdapter.java
@@ -4,19 +4,17 @@ import com.dongni.dongnimall.interceptor.TokenInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author cengshuai on 2019-09-05.
  * @version 1.0
  */
 @Configuration
-public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
+public class MyWebMvcConfigurerAdapter implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(getTokenInterceptor()).addPathPatterns("/home/**","/menu/**","/order/**","/user/**");
-        super.addInterceptors(registry);
     }
 
     @Bean
