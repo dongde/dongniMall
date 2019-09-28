57041959edcbd46da4170e44a6f1b86e9d7084db
换了马甲的小强 <569790480@qq.com>
2019-09-28 22:34:24 +0800

    提交一个版本的前端

--- src/dongniMall/src/main/java/com/dongni/dongnimall/MyWebMvcConfigurerAdapter.java
+++ src/dongniMall/src/main/java/com/dongni/dongnimall/MyWebMvcConfigurerAdapter.java
@@ -4,6 +4,7 @@ import com.dongni.dongnimall.interceptor.TokenInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -15,6 +16,12 @@ public class MyWebMvcConfigurerAdapter implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(getTokenInterceptor()).addPathPatterns("/home/**","/menu/**","/order/**","/user/**");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:/dist/index.html#/home");
+        registry.addViewController("/admin").setViewName("forward:/index.html");
     }
 
     @Bean
